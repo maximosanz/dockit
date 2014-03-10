8034f70902f0ee7c880c089b10a544ecc45d50db
Maximo Sanz Hernandez <maximo.sanzh@gmail.com>
2014-03-10 16:16:23 +0000

    Max moves to server 2

--- all/urls.py
+++ all/urls.py
@@ -5,7 +5,7 @@ from all import views
 urlpatterns = patterns('',
     url(r'^$', views.search, name='search'),
     url(r'^summary_(?P<target>\w+)/$', views.summary, name='summary'),
-    url(r'^target_models_(?P<name>\w+)/$', views.target_models, name='target_models'),
+    url(r'^target_models_(?P<name>[\S|^\_]+)_(?P<method>[\S|^\_]+)_(?P<refinement>[\S|^\_]+)_(?P<i_rmsd_threshold>[\S|^\_]+)_(?P<l_rmsd_threshold>[\S|^\_]+)_(?P<r_rmsd_threshold>[\S|^\_]+)_(?P<fnat_threshold>[\S|^\_]+)/$', views.target_models, name='target_models'),
     url(r'^target/$', views.target, name='target'),
     url(r'^target/(?P<name>\w+)/$', views.target_info, name='target_info'),
     url(r'^model_select_(?P<target>[\S|^\_]+)_(?P<method>[\S|^\_]+)_(?P<refinement>[\S|^\_]+)_(?P<i_rmsd_threshold>[\S|^\_]+)_(?P<l_rmsd_threshold>[\S|^\_]+)_(?P<r_rmsd_threshold>[\S|^\_]+)_(?P<fnat_threshold>[\S|^\_]+)/$', views.model_select, name='model_select'),
